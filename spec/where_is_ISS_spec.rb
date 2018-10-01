344420c0dda0fa8c6040f150bea1ad165d9a28c4
Manoj Naidu <manojnaidu619@gmail.com>
2018-10-01 22:08:58 +0530

    First test run

--- spec/where_is_ISS_spec.rb
+++ spec/where_is_ISS_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe WhereIsISS do
-  it "has a version number" do
-    expect(WhereIsISS::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+    it "has a version number" do
+      expect(WhereIsISS::VERSION).not_to be nil
+    end
+
+    it "checks for class" do
+      expect(Iss).to be_a_kind_of Class
+    end
+
 end
