086a2e5a89d772caeb3b1cd2576ab6ff752ec42b
Manoj Naidu <manojnaidu619@gmail.com>
2018-10-02 19:44:24 +0530

    Tests updated

--- spec/where_is_ISS_spec.rb
+++ spec/where_is_ISS_spec.rb
@@ -9,12 +9,6 @@ RSpec.describe WhereIsISS do
       expect(Iss).to be_a_kind_of Class
     end
 
-    context "Response test" do
-      it "Response status" do
-        @data = JSON[open('https://api.wheretheiss.at/v1/satellites/25544').read]
-        expect(@data).not_to be_empty
-      end
-
       it "Checks for data type of latitude and longitude" do
           @data = JSON[open('https://api.wheretheiss.at/v1/satellites/25544').read]
           expect(@data['latitude']).to be_a(Float)
@@ -25,6 +19,8 @@ RSpec.describe WhereIsISS do
         response = Net::HTTP.get_response(URI.parse("https://api.wheretheiss.at/v1/satellites/25544"))
            expect((response.code).to_i).to be_a(Integer)
            expect((response.code).to_i).to equal 200 or expect((response.code).to_i).to equal 404
+           @data = JSON[open('https://api.wheretheiss.at/v1/satellites/25544').read] if (response.code).to_i == 200
+           expect(@data).not_to be_empty
       end
 
       it "checks for Location of ISS if response is 200 OK" do
@@ -33,8 +29,7 @@ RSpec.describe WhereIsISS do
           @iss = JSON[open("https://api.wheretheiss.at/v1/coordinates/#{@lat},#{@long}").read] if (response.code).to_i == 200
           expect(@data).not_to be nil
           expect(@iss['timezone_id']).not_to be_empty if (response.code).to_i == 200
-      end
-    end
+       end
 
     context "Response data type check" do
 
@@ -53,6 +48,18 @@ RSpec.describe WhereIsISS do
         end
 
     end
+
+      it "validates response and response type" do
+          @validate = Net::HTTP.get_response(URI.parse('http://api.open-notify.org/astros.json'))
+          @resp = @validate.code.to_i
+          expect(@resp).to equal 200
+          @response = JSON[open('http://api.open-notify.org/astros.json').read] if @resp == 200
+          expect(@response).not_to be_empty if @resp == 200
+          expect(@response['number']).to be_a(Fixnum)
+           @response['people'].each do |resp|
+              expect(resp['name']).to be_a(String)
+           end
+       end
 
 
 end
